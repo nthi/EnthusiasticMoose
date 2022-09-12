a5a6bdede8da72d60bace80a24336e980226e23e
Natalie Thiele <natalie.a.a.thiele@gmail.com>
2022-09-12 13:38:24 -0400

    Refactors project to add new questions and call them from Main()

--- Program.cs
+++ Program.cs
@@ -14,7 +14,17 @@ namespace EnthusiasticMoose
             MooseSays("H I, I'M  E N T H U S I A S T I C !");
             MooseSays("I really am enthusiastic");
 
-            // Ask a question
+            // Ask some questions
+            CanadaQuestion();
+            EnthusiasticQuestion();
+            LoveCSharpQuestion();
+            SecretQuestion();
+
+        }
+
+
+        static void CanadaQuestion()
+        {
             bool isTrue = MooseAsks("Is Canada real?");
             if (isTrue)
             {
@@ -24,8 +34,47 @@ namespace EnthusiasticMoose
             {
                 MooseSays("I  K N E W  I T !!!");
             }
-
         }
+
+        static void EnthusiasticQuestion()
+        {
+            bool isEnthusiastic = MooseAsks("Are you enthusiastic?");
+            if (isEnthusiastic)
+            {
+                MooseSays("Yay!");
+            }
+            else
+            {
+                MooseSays("You should try it!");
+            }
+        }
+
+        static void LoveCSharpQuestion()
+        {
+            bool doesLoveCSharp = MooseAsks("Do you love C# yet?");
+            if (doesLoveCSharp)
+            {
+                MooseSays("Good job sucking up to your instructor!");
+            }
+            else
+            {
+                MooseSays("You will...oh, yes, you will...");
+            }
+        }
+
+        static void SecretQuestion()
+        {
+            bool isEverythingFine = MooseAsks("Do you want to know a secret?");
+            if (isEverythingFine)
+            {
+                MooseSays("ME TOO!!!! I love secrets...tell me one!");
+            }
+            else
+            {
+                MooseSays("Oh, no...secrets are the best, I love to share them!");
+            }
+        }
+
         static void MooseSays(string message)
         {
             Console.WriteLine($@"
